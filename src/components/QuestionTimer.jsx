378f3458084f48868b18bbcd36fb6fc26d45b19f
Trishna <trishna.patil13@gmail.com>
2024-04-13 14:30:07 -0400

    adding key prop tp timer component to get new timers for every question

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,16 +4,31 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    setTimeout(onTimeout, timeout);
+    console.log("TIMOUT");
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      console.log("clearing timer...");
+      clearTimeout(timer);
+    };
+    /* the clean up function (mentioned inside return) will automatically be executed by react 
+    before the useEffect runs again or when the component is unmounted from the DOM (clears from the screen)*/
   }, [timeout, onTimeout]);
   // we also need to put setTimeout inside useEffect because since we are updating the state in setIntervals, component will get re-rendered
   // and we'll end up having multiple timers otherwise!
   // dependencies inside useEffect are the state or prop values that the function is dependent upon
 
   useEffect(() => {
-    setInterval(() => {
+    console.log("INTERVAL");
+    const interval = setInterval(() => {
       setRemainingTime((prevTime) => prevTime - 100);
     }, 100);
+
+    return () => {
+      //clean up function
+      console.log("clearing interval...");
+      clearInterval(interval);
+    };
   }, []);
   // need to use setInterval inside useEffect because since we are updating the state inside it,
   // we will end up inside an infite loop of state updates and re-renders
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../questions";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer";
@@ -22,16 +22,30 @@ export default function Quiz() {
   //making a copy first, because sort() will not make a new copy and i need the original answers order unchanged
   shuffledAnswers.sort(() => Math.random() - 0.5); //50% negative values, 50% positive values
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   return (
     <div id="quiz">
       <div id="question">
+        {/* QuestionTimer component does not get recreated without the key prop */}
         <QuestionTimer
+          key={activeQuestionIndex}
+          // whenever the key prop changes on a component,
+          // react will destroy the old component instance and create a new one
+          // it unmounts and remounts it
+          // that's how we can get new timer for every new question
           timeout={10000}
           onTimeout={() => {
             handleSelectAnswer(null);
@@ -41,9 +55,7 @@ export default function Quiz() {
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
+              <button onClick={handleSkipAnswer}>{answer}</button>
             </li>
           ))}
         </ul>
